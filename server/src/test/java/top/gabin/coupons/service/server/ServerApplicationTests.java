8a187f4f468a8b947fd6a1ebaae60ff4a5f78fef
gabin <18649660797@163.com>
2020-07-31 23:09:37 +0800

    init

---
+++ server/src/test/java/top/gabin/coupons/service/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.gabin.coupons.service.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
