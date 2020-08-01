c08add88c0574f395220ac9de1667e78afc3a405
gabin <18649660797@163.com>
2020-08-01 21:18:57 +0800

    使用consul做服务注册中心-中间件模式

--- provider/src/main/java/top/gabin/coupons/service/provider/ProviderApplication.java
+++ provider/src/main/java/top/gabin/coupons/service/provider/ProviderApplication.java
@@ -2,10 +2,10 @@ package top.gabin.coupons.service.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProviderApplication {
 
 	public static void main(String[] args) {
