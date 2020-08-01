80ed843655247aef4515c3ad95198913801a80c9
gabin <18649660797@163.com>
2020-08-02 00:20:02 +0800

    集成turbine监控

--- server/src/test/java/top/gabin/coupons/service/server/ServerApplicationTests.java
+++ server/src/test/java/top/gabin/coupons/service/server/ServerApplicationTests.java
@@ -1,6 +1,6 @@
 package top.gabin.coupons.service.server;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
